ac87524e1370db24356f2888a36ebc7e4e78919f
Corey Chen <greenteay2k@hotmail.com>
2023-07-27 22:51:21 -0700

    adding more api and update ingredient

--- routes/recipes.js
+++ routes/recipes.js
@@ -6,8 +6,8 @@ const { readRecipes } = require("../utils/helper.js");
 router.get("/", (req, res) => {
     // const recipe = readRecipe();
     const listRecipes = readRecipes().map((recipe) => {
-        const { id, name, main_ingredient } = recipe;
-        return { id, name, main_ingredient };
+        const { id, name, main_ingredient, ingredients } = recipe;
+        return { id, name, main_ingredient, ingredients };
     });
     res.status(200).json(listRecipes);
 });
